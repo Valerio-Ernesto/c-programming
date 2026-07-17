16ba250acc33b99a9b4f6eea476a5a1835b5f800
Valerio <aspiring.mathematician.1729@gmail.com>
2026-07-16 20:43:31 -0700

    Nueva documentación en devlog.wiki

---
+++ test/ninitvalues.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(void)
+{
+	int first, second, third;
+	float randomOne, arbitrary, variable;
+
+	printf("Estas variables no fueron inicializadas.\n");
+	printf("Números enteros: %d, %d y %d.\n", first, second, third);
+	printf("Números reales: %f, %f y %f.", randomOne, arbitrary, variable);
+
+	return 0;
+}
